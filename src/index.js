46b40cf566ac75986079d892181f18235aef44e6
Max Pietsch <maxwell.pietsch@gmail.com>
2021-06-15 17:21:17 -0700

    replaced React with my test page

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+class Max extends React.Component {
+  render() {
+    return (
+      <div className="max">
+        <h1>title 1</h1>
+        <h2>title2</h2>
+      </div>
+    );
+  }
+}
+
+
+// ========================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Max />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
